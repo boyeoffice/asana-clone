6a2a56e961084e0282bb4145848f8af01a4b70b4
Olakunle Boye <kunlexzy@gmail.com>
2022-06-14 22:05:16 +0100

    Added: asana and google icon

--- plugins/dateformat.js
+++
@@ -1,9 +0,0 @@
-import Vue from "vue"
-
-Vue.filter("dateformat", value => {
-  return new Date(value).toLocaleDateString("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric"
-  })
-})
