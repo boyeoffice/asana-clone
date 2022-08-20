695d102565564ff92c6a41c488f9abdbb288f284
Olakunle Boye <kunlexzy@gmail.com>
2022-08-20 23:22:47 +0100

    implementing auth footer

--- src/layouts/Auth.vue
+++ src/layouts/Auth.vue
@@ -3,8 +3,20 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-<div>
-  This is auth layout
+<div class="login_footer">
+  <nav>
+    <ul class="login_footer_row">
+      <li>
+        <a href="#">Asana.com</a>
+      </li>
+    </ul>
+  </nav>
 </div>
 <RouterView />
 </template>
+
+<style lang="css">
+.login_footer {
+  color: var(--color-tweak);
+}
+</style>
