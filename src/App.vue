bc5fe8984662244ae2f596cbd24735029bf042b7
Olakunle Boye <kunlexzy@gmail.com>
2022-08-21 05:05:08 +0100

    Added: footer links

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,8 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/auth/login">Login</RouterLink>
-      </nav>
+  <div class="">
     </div>
-  </header>
 
   <RouterView />
 </template>
--- src/layouts/Auth.vue
+++ src/layouts/Auth.vue
@@ -1,21 +1,44 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue'
+
+const firstItems= ref([
+  'Asana.com',
+  'Supports',
+  'Integration',
+  'Forum'
+])
 </script>
 
 <template>
-<div class="login_footer">
-  <nav>
-    <ul class="login_footer_row">
-      <li>
-        <a href="#">Asana.com</a>
-      </li>
-    </ul>
-  </nav>
+<div class="auth_card">
+  <RouterView />
+  <div class="login_footer">
+    <nav>
+      <ul class="login_footer_row">
+        <li v-for="(item,index) in firstItems" :key="index">
+          <a href="#">{{ item }}</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </div>
-<RouterView />
 </template>
 
 <style lang="css">
+.auth_card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.login_footer {
+  display: flex;
+  margin-top: 48px;
+  align-items: center;
+}
+.login_footer_row {
+  display: flex;
+}
 .login_footer {
   color: var(--color-tweak);
 }
