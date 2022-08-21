b0f066b73f9081518d344be58bfda0ba7ad09581
Olakunle Boye <kunlexzy@gmail.com>
2022-08-21 06:56:48 +0100

    styled footer items

--- src/layouts/Auth.vue
+++ src/layouts/Auth.vue
@@ -6,7 +6,17 @@ const firstItems= ref([
   'Asana.com',
   'Supports',
   'Integration',
-  'Forum'
+  'Forum',
+  'Developers & API'
+])
+
+const secondItems = ref([
+  'Resources',
+  'Guide',
+  'Templates',
+  'Pricing',
+  'Terms',
+  'Privacy'
 ])
 </script>
 
@@ -16,8 +26,13 @@ const firstItems= ref([
   <div class="login_footer">
     <nav>
       <ul class="login_footer_row">
-        <li v-for="(item,index) in firstItems" :key="index">
-          <a href="#">{{ item }}</a>
+        <li class="f_login_item" v-for="(item,index) in firstItems" :key="index">
+          <a class="tweak" href="#">{{ item }}</a>
+        </li>
+      </ul>
+      <ul class="login_footer_row">
+        <li class="f_login_item" v-for="(item,index) in secondItems" :key="index">
+          <a class="tweak" href="#">{{ item }}</a>
         </li>
       </ul>
     </nav>
@@ -31,14 +46,31 @@ const firstItems= ref([
   flex-direction: column;
   align-items: center;
 }
+
 .login_footer {
   display: flex;
   margin-top: 48px;
   align-items: center;
 }
+
 .login_footer_row {
-  display: flex;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-left: 8px;
+    margin-right: 8px;
+    margin-top: 8px;
 }
+
+.login_footer_row:first-child {
+  margin-top: 0;
+}
+
+.f_login_item+.f_login_item::before {
+    content: "•";
+    margin: 0 4px;
+}
+
 .login_footer {
   color: var(--color-tweak);
 }
