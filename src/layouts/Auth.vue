3cd26b15c1776fa2e600c6b1623eb0459c034981
Olakunle Boye <kunlexzy@gmail.com>
2022-08-22 11:23:46 +0100

    redesigning login ui

--- src/layouts/Auth.vue
+++ src/layouts/Auth.vue
@@ -1,3 +1,61 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue'
+
+const firstItems= ref([
+  'Asana.com',
+  'Supports',
+  'Integration',
+  'Forum',
+  'Developers & API'
+])
+
+const secondItems = ref([
+  'Resources',
+  'Guide',
+  'Templates',
+  'Pricing',
+  'Terms',
+  'Privacy'
+])
+</script>
+
 <template>
-  <div class="flex"></div>
+  <div class="flex flex-col items-center">
+    <RouterView />
+    <div
+      class="flex items-center"
+      style="margin-top: 48px;">
+      <nav>
+        <ul class="flex flex-wrap justify-center" style="margin: 8px;">
+          <li class="f_login_item" v-for="(item,index) in firstItems" :key="index">
+            <a class="tweak" href="#">{{ item }}</a>
+          </li>
+        </ul>
+        <ul class="flex flex-wrap justify-center" style="margin: 0 8px;">
+          <li class="f_login_item" v-for="(item,index) in secondItems" :key="index">
+            <a class="tweak" href="#">{{ item }}</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
 </template>
+
+<style lang="css">
+  .f_login_item+.f_login_item::before {
+    content: "•";
+    margin: 0 4px;
+  }
+
+  .f_login_item a {
+    font-size: 14px;
+    line-height: 22px;
+  }
+
+.f_login_item a:hover {
+  color: var(--color-primary-text-hover);
+  fill: var(--color-primary-text-hover);
+  background-color: transparent;
+}
+</style>
--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,3 +1,27 @@
 <template>
-  <div class="flex"></div>
+  <div class="login_card_layout">
+    <div class="w-full">
+      <div class="flex flex-col">
+        <a href="#" class="flex items-center self-center flex-col primary_link" style="margin-bottom: 48px;">
+          <img src="@/assets/logo.png" style="height: 48px;" />
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style lang="css">
+.login_card_layout {
+  background-color: var(--color-card-background);
+  border-radius: 8px;
+  box-shadow: var(--color-box-shadow-large);
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 80px;
+  max-width: 440px;
+  padding: 48px 32px;
+  width: calc(100% - 16px);
+}
+</style>
