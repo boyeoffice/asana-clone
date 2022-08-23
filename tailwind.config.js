704306ad1624f1a15d34bd0f01f4cb2688f4010a
Olakunle Boye <kunlexzy@gmail.com>
2022-08-23 02:07:39 +0100

    updated google button

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'body': ['-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Helvetica,Arial,sans-serif']
+      }
+    },
   },
   plugins: [],
 }
