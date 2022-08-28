c9ce1823614714fe8aa899e6cd5e27f1ed9a1edf
Olakunle Boye <kunlexzy@gmail.com>
2022-08-28 18:30:59 +0100

    Added: password field

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
   import { ref } from 'vue'
+
+  interface Form {
+    email: string,
+    password?: string
+  }
   const isFirst = ref(false)
+
+  const form = ref<Form>({ email: 'user@example.com' })
 </script>
 
 <template>
@@ -17,19 +24,43 @@
         </a>
       </div>
       <h2 class="text-center text-3xl color-text font-normal" style="margin-bottom: 32px;">Login in to Asana</h2>
-      <div class="flex google_button w-full">
+      <div v-show="isFirst" class="flex google_button w-full">
         <svg class="google_icon h-5 invisible md:visible" viewBox="0 0 18 18"><path d="M17.64,9.20454545 C17.64,8.56636364 17.5827273,7.95272727 17.4763636,7.36363636 L9,7.36363636 L9,10.845 L13.8436364,10.845 C13.635,11.97 13.0009091,12.9231818 12.0477273,13.5613636 L12.0477273,15.8195455 L14.9563636,15.8195455 C16.6581818,14.2527273 17.64,11.9454545 17.64,9.20454545 L17.64,9.20454545 Z" fill="#4285F4"></path><path d="M9,18 C11.43,18 13.4672727,17.1940909 14.9563636,15.8195455 L12.0477273,13.5613636 C11.2418182,14.1013636 10.2109091,14.4204545 9,14.4204545 C6.65590909,14.4204545 4.67181818,12.8372727 3.96409091,10.71 L0.957272727,10.71 L0.957272727,13.0418182 C2.43818182,15.9831818 5.48181818,18 9,18 L9,18 Z" fill="#34A853"></path><path d="M3.96409091,10.71 C3.78409091,10.17 3.68181818,9.59318182 3.68181818,9 C3.68181818,8.40681818 3.78409091,7.83 3.96409091,7.29 L3.96409091,4.95818182 L0.957272727,4.95818182 C0.347727273,6.17318182 0,7.54772727 0,9 C0,10.4522727 0.347727273,11.8268182 0.957272727,13.0418182 L3.96409091,10.71 L3.96409091,10.71 Z" fill="#FBBC05"></path><path d="M9,3.57954545 C10.3213636,3.57954545 11.5077273,4.03363636 12.4404545,4.92545455 L15.0218182,2.34409091 C13.4631818,0.891818182 11.4259091,0 9,0 C5.48181818,0 2.43818182,2.01681818 0.957272727,4.95818182 L3.96409091,7.29 C4.67181818,5.16272727 6.65590909,3.57954545 9,3.57954545 L9,3.57954545 Z" fill="#EA4335"></path></svg>
         <span>Continue with Google</span>
       </div>
-      <span class="flex items-center text-center">
+      <span v-show="isFirst" class="flex items-center text-center">
         <span class="SeparatorRow-horizontalLine"></span>
         <span style="margin: 0 8px;" class="text-base">or</span>
         <span class="SeparatorRow-horizontalLine"></span>
       </span>
       <form class="flex flex-col mt-8">
-        <label for="Email" class="color-tweak" style="font-size: 12px; line-height: 18px;">Email address</label>
+        <div class="_default flex mb-10 py-3 px-6 justify-start items-center w-full color-text rounded-md">
+          <div class="flex w-full items-center justify-center">
+            <img class="pr-2" src="@/assets/grey_man_2_avatar.png" width="28" height="28">
+            <span class="text-base font-normal">{{ form.email }}</span>
+          </div>
+          <div>
+            <svg class="CompoundIcon--xsmall CompoundIcon XCircleCompoundIcon" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M23,12c0,6.1-4.9,11-11,11S1,18.1,1,12S5.9,1,12,1S23,5.9,23,12z" class="CompoundIcon-outer"></path><path d="M13.4,12l4.3-4.3c0.4-0.4,0.4-1,0-1.4s-1-0.4-1.4,0L12,10.6L7.7,6.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l4.3,4.3l-4.3,4.3c-0.4,0.4-0.4,1,0,1.4C6.5,17.9,6.7,18,7,18s0.5-0.1,0.7-0.3l4.3-4.3l4.3,4.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3c0.4-0.4,0.4-1,0-1.4L13.4,12z" class="CompoundIcon-inner"></path></svg>
+          </div>
+        </div>
+        <label
+          :for="isFirst === true ? 'Email':'Password'"
+          class="color-tweak"
+          style="font-size: 12px; line-height: 18px;">
+            {{ isFirst === true ? 'Email address':'Password' }}
+        </label>
         <div class="pt-2">
-          <input type="email" class="form-input px-3 py-1 rounded-md w-full" placeholder="name@company.com">
+          <input
+            v-show="isFirst"
+            type="email"
+            class="form-input px-3 py-1 rounded-md w-full"
+            placeholder="name@company.com"
+            v-model="form.email">
+          <input
+            v-show="!isFirst"
+            type="password"
+            class="form-input px-3 py-1 rounded-md w-full"
+            v-model="form.password">
         </div>
         <div class="pt-4">
           <button v-show="isFirst" @click="isFirst=!isFirst" type="button" class="NuxButton ThemeableRectangularButton w-full py-2 rounded-lg">Continue</button>
