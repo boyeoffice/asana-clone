ba6f47cbf178190a66a49c6a4891fe6d44765674
Olakunle Boye <kunlexzy@gmail.com>
2022-09-03 12:45:19 +0100

    implementing header

--- src/components/Main/HomeHeader.vue
+++ src/components/Main/HomeHeader.vue
@@ -1,3 +1,14 @@
 <template>
-  <nav class=""></nav>
+  <nav class="flex w-full px-4 items-center h-16 justify-between">
+    <div class="flex w-full">
+      <div class="flex items-center">
+        <h1 class="color-text font-medium text-lg">Home</h1>
+      </div>
+    </div>
+    <div class="flex items-center">
+      <div class="flex">
+        <input type="text" class="form-input px-3 py-1 rounded-md" >
+      </div>
+    </div>
+  </nav>
 </template>
--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
   import { RouterView } from 'vue-router'
   import SidebarVue from '@/components/Main/Sidebar.vue';
-import Sidebar from '../components/Main/Sidebar.vue';
+  import Sidebar from '../components/Main/Sidebar.vue';
+  import HomeHeader from '../components/Main/HomeHeader.vue'
 </script>
 
 <template>
   <div class="dashboard full_page flex flex-initial ">
-    <Sidebar />
-    <RouterView />
+    <div class="flex">
+      <Sidebar />
+    </div>
+    <div class="flex flex-col">
+      <HomeHeader />
+      <RouterView />
+    </div>
   </div>
 
 </template>
